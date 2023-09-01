ec277a3372bb91c249c74474b03b0a4bcb2b7c67
root <root@KZALA01ITS004.e-globalink.net>
2023-09-02 01:41:49 +0600

    finish step 3

---
+++ src/Cli.php
@@ -0,0 +1,12 @@
+<?php
+namespace Php\Project\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function sayHello(): void
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
